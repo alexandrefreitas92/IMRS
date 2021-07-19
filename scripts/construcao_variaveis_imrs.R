80412917131393a9a5a70b801a88aa74ce983e27
Alexandre Freitas <alexandrefreitas92@gmail.com>
2021-07-19 17:50:31 -0300

    atualizando

---
+++ scripts/construcao_variaveis_imrs.R
@@ -0,0 +1,15 @@
+# Informação Geral --------------------------------------------------------
+
+# Script de atualização automática das informações do IMRS da área de Assistência Social
+# Alexandre Freitas
+# Email: alexandre.freitas@fjp.mg.gov.br
+# Data de construção: Julho e agosto de 2021.
+
+# Ler bibliotecas ---------------------------------------------------------
+
+library(tidyverse)
+library(openxlsx)
+
+# Ler base de dados -------------------------------------------------------
+
+
